5d355aabd5294c4b7fb4d26865284fc1ce146eff
wonmijin <mijin950503@gmail.com>
2024-02-22 22:55:23 +0900

    Add Serverless Functions

--- src/store/about.js
+++ src/store/about.js
@@ -5,5 +5,5 @@ export default new Store({
     name: 'WONMIJIN',
     email: 'mijin950503@gmail.com',
     github: 'https://github.com/wonmijin',
-    repository: 'https://github.com/wonmijin'
+    repository: 'https://github.com/wonmijin/MOVIE'
 })
